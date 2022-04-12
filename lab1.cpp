e181fb072cfbed2e976f1ce548a56e19389bf5b0
MagometS <magik200219@gmail.com>
2022-04-12 23:38:36 +0300

    Добавить функцию получения коэффициентов уравнения

---
+++ lab1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std; 
+
+int main()
+{
+
+
+	return 0;
+}
+
+
+void GetCoeff(double& a,double& b, double& c)
+{
+	cout << "Введите коэффициент a";
+
+}
