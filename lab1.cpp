6da72a2c1223ffb1910c69f94ea10bc65519a736
MagometS <magik200219@gmail.com>
2022-04-13 03:15:05 +0300

    Исправить ошибки в функциях; Рабочая версия программы

--- lab1.cpp
+++ lab1.cpp
@@ -6,13 +6,15 @@ using namespace std;
 
 void GetCoeff(double& a, double& b, double& c);
 double Discriminant(double a, double b, double c);
+void FindRoots(double** x1, double** x2, double a, double b, double c);
+void PrintResults(double* x1, double* x2);
+void SolveEquation();
+
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	double a, b, c;
-	GetCoeff(a, b, c);
-	cout << a << "," << b << "," << c;
+	SolveEquation();
 
 	return 0;
 }
@@ -36,40 +38,51 @@ double Discriminant(double a, double b, double c)
 }
 
 //Находит корни
-void FindRoots(double* x1, double* x2, double a, double b, double d)
+void FindRoots(double** x1, double** x2, double a, double b, double c)
 {
+	double d = Discriminant(a, b, c);
 	if (d > 0)
 	{
-		*x1 = (sqrt(d) - b) / (2 * a);
-		*x2 = ((-b) - sqrt(d)) / (2 * a);
+		**x1 = (sqrt(d) - b) / (2 * a);
+		**x2 = ((-b) - sqrt(d)) / (2 * a);
 	}
 	if (d == 0)
 	{
-		*x1 = (-b) / (2 * a);
-		*x2 = *x1;
+		**x1 = (-b) / (2 * a);
+		**x2 = **x1;
 	}
 	if (d < 0)
 	{
-		x1 = NULL;
-		x2 = NULL;
+		*x1 = NULL;
+		*x2 = NULL;
 	}
 }
 
-
-void PrintResults(double x1, double x2)
+//Выводит корни уравнения
+void PrintResults(double* x1, double* x2)
 {
 	if (x1 == NULL)
 	{
 		cout << "нет действительных корней" << endl;
 		return;
 	}
-	if (x1 == x2)
+	if (*x1 == *x2)
 	{
-		cout << "x = " << x1 << endl;
+		cout << "x = " << *x1 << endl;
 		return;
 	}
+	cout << "x1 = " << *x1 << endl << "x2 = "<< *x2 << endl;
+}
 
-	cout << "x1 = " << x1 << endl << "x2 = "<< x2 << endl;
-	
-
+//Работа программы
+void SolveEquation()
+{
+	double a, b, c;
+	double* x1 = new double;
+	double* x2 = new double;
+	GetCoeff(a, b, c);
+	FindRoots(&x1, &x2, a, b, c);
+	PrintResults(x1, x2);
+	delete x1;
+	delete x2;
 }
